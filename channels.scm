0e8f2407a3f545cae434db0fe6b41003a61a47f6
Nabeel Javed <nbljaved96@gmail.com>
2024-03-10 22:01:35 +0530

    Try to fix channel configuration.

--- channels.scm
+++ channels.scm
@@ -15,4 +15,7 @@
           "257cebd587b66e4d865b3537a9a88cccd7107c95"
           (openpgp-fingerprint
            "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+       (channel
+        (name 'nbl)
+        (url "https://github.com/nbljaved96/guix-config"))
        %default-channels)
