3db2f311157717f9dde5e41c775cbb261f716927
Nabeel Javed <nbljaved96@gmail.com>
2025-06-30 19:16:42 +0530

    use typst, add some comments abow running curl in guix shell

--- channels.scm
+++ channels.scm
@@ -15,6 +15,22 @@
           "257cebd587b66e4d865b3537a9a88cccd7107c95"
           (openpgp-fingerprint
            "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+       (channel
+        (name 'guix-science)
+        (url "https://codeberg.org/guix-science/guix-science.git")
+        (introduction
+         (make-channel-introduction
+          "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+          (openpgp-fingerprint
+           "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+       (channel
+        (name 'guix-science-nonfree)
+        (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+        (introduction
+         (make-channel-introduction
+          "58661b110325fd5d9b40e6f0177cc486a615817e"
+          (openpgp-fingerprint
+           "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
        ;; (channel
        ;;  (name 'nbl)
        ;;  (url (string-append "file://" (getenv "HOME")
--- scripts/uv-guix-shell.sh
+++ scripts/uv-guix-shell.sh
@@ -3,4 +3,8 @@
 ## Example setting
 # alias uv="$HOME/guix-config/scripts/uv-guix-shell.sh"
 
+# `curl` requires `nss-certs`
+# guix shell -CNF -E PATH --share=$HOME gcc-toolchain bash which coreutils curl nss-certs -- curl blah-blah....
+
+# to run uv
 guix shell -CNF -E PATH --share=$HOME gcc-toolchain bash which coreutils  -- uv "$@"
